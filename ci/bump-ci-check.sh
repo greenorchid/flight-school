3ebe9b1cf8480fcc5dc0434fa63a02cb995cdc12
Marc <@greenorchid>
2017-08-04 10:59:10 +0100

    change pipeline to bump version after ci

---
+++ ci/bump-ci-check.sh
@@ -0,0 +1,10 @@
+git clone flight-school updated-repo
+
+cd updated-repo
+echo CI checked at $(date) >> ci/ci_passed
+
+git config --global user.email "nobody@concourse.ci"
+git config --global user.name "Concourse"
+
+git add -a ci/ci_passed
+git commit -m "CI check passed."
